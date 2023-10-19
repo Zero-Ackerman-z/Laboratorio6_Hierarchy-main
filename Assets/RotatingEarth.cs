c79b2fe2d63e2d18b6e274b990c1f6e24ba9344f
Zero-Ackerman-z <delia.huayas@tecsup.edu.pe>
2023-10-19 09:47:20 -0500

    Initial commit

---
+++ Assets/RotatingEarth.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingEarth : MonoBehaviour
+{
+    public float rotationSpeed = 10.0f; // Velocidad de rotación del Sol en grados por segundo
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+
+    }
+}
